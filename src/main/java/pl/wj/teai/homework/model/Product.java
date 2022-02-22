a860a4829242b6161760db635a9389cfa81843fd
Wiktoria Janiaczyk <wiktoria.janiaczyk@gmail.com>
2022-02-22 17:52:48 +0100

    Add product

---
+++ src/main/java/pl/wj/teai/homework/model/Product.java
@@ -0,0 +1,30 @@
+package pl.wj.teai.homework.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String name;
+
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
